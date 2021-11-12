4d403c78777d7280086aed3b6db8679b0d7e2fa0
JuanKier <juanandreskl1999@gmail.com>
2021-11-11 21:10:24 -0300

    vent advertencia eliminar registro y unique

--- Pagina/models.py
+++ Pagina/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key = True)
-    nombre_usuario = models.CharField(max_length = 10)
+    nombre_usuario = models.CharField(max_length = 10, unique = True)
     password_usuario = models.CharField(max_length = 10)
     nombre_completo_usuario = models.CharField(max_length = 200)
